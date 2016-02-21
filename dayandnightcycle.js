dd31091086dbe46f6e88c32a5057180da6a028aa
Samuel <Samuel_ipad2@hotmail.com>
2016-02-22 07:46:31 +1100

    A layout

---
+++ dayandnightcycle.js
@@ -0,0 +1,20 @@
+var images = new Array();
+images[0] = "bg%20afternoon.jpg";
+images[1] = "bg%20night.jpg";
+images[2] = "bg%20day.jpg";
+for (var i = 2; i < 11; i++) {
+    images.push("img" + i + ".gif");
+}
+var x = 0;
+
+function changeImage() {
+    document.getElementById('ad').src = images[x];
+    if (x < 1) {
+        x += 1;
+    } else {
+        x = 0;
+    }
+}
+setInterval(function () {
+    changeImage();
+}, 30000); /*Every 5 min*/
