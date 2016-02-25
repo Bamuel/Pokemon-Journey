fe54dbcd786368f1c83eaf2ac06ddfa4f59e0f39
Samuel <Samuel_ipad2@hotmail.com>
2016-02-26 08:15:47 +1100

    battle.php update

--- battle.php
+++ battle.php
@@ -41,11 +41,9 @@ else{
         else if(event.ctrlKey && event.shiftKey && event.keyCode==73) {
             return false;  //Prevent from ctrl+shift+i
         }
-        /*
          else if(event.ctrlKey && event.keyCode==85) {
          return false;  //Prevent from ctrl+u
          }
-         */
         else if(event.keyCode==123){
             return false; //Prevent from F12
         }
