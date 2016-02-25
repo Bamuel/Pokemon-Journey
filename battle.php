5667df96352eb4f30794977279ab5d7c71c9d6a3
Samuel <Samuel_ipad2@hotmail.com>
2016-02-25 23:15:34 +1100

    battle

--- battle.php
+++ battle.php
@@ -846,6 +846,8 @@ $pokemon = array(
     'pokemonsprites/zweilous.gif',
     'pokemonsprites/zygrade.gif'
 );
+?>
+<?php
 
 $ip = "$_SERVER[REMOTE_ADDR]";
 if($ip === "127.0.0.1"  || $ip === "58.172.56.231") { /*Only awesome people can have spacial field*/
@@ -885,26 +887,26 @@ else{
             return false;  //Prevent from ctrl+shift+i
         }
         /*
-        else if(event.ctrlKey && event.keyCode==85) {
-            return false;  //Prevent from ctrl+u
-        }
-        */
+         else if(event.ctrlKey && event.keyCode==85) {
+         return false;  //Prevent from ctrl+u
+         }
+         */
         else if(event.keyCode==123){
             return false; //Prevent from F12
         }
     };
 </script>
-    <img id="bg" src="<?php echo $field[rand(0, count($field) - 1)]; ?>">
-    <img id="pokemon" src="<?php echo $pokemon[rand(0, count($pokemon) - 1)]; ?>">
-    <div id="menu">
-        <div>
-            <button id="" onclick="window.close()">Fight</button>
-            <button id="" onclick="window.close()">Throw Bait</button>
-        </div>
-        <div>
-            <button id="" onclick="window.close()">Throw Pokeball</button>
-            <button id="" onclick="window.close()">Run</button>
-        </div>
+<img id="bg" src="<?php echo $field[rand(0, count($field) - 1)]; ?>">
+<img id="pokemon" src="<?php echo $pokemon[rand(0, count($pokemon) - 1)]; ?>">
+<div id="menu">
+    <div>
+        <button id="" onclick="window.close()">Fight</button>
+        <button id="" onclick="window.close()">Throw Bait</button>
     </div>
+    <div>
+        <button id="" onclick="window.close()">Throw Pokeball</button>
+        <button id="" onclick="window.close()">Run</button>
+    </div>
+</div>
 </body>
 </html>
