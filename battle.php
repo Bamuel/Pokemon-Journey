b26106fccbcf7d9c6bd858282c7a510343e879fd
Samuel <Samuel_ipad2@hotmail.com>
2016-04-15 15:54:52 +1000

    cookies is an issue

--- battle.php
+++ battle.php
@@ -43,6 +43,18 @@ $opp22 = str_replace('-sunshine', '', $opp21);
 $opp23 = str_replace('-zen', '', $opp22);
 $opplast= ucfirst($opp23);
 ?>
+<?php
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
