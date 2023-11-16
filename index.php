60781a80977f1644fa873f1aa41a431d4b5b614b
Bamuel <me@bamuel.com>
2023-11-16 16:14:37 +1100

    mobile overflow fix

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
 }
 ?>
 <style>
+    body, html {
+        overflow: hidden;
+    }
+
     #menu_buttons {
         position: absolute;
         top: 70px;
